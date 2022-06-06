b3c1e8353db2da32d5ddaa59aab93443f925c967
Renata Santos <renatados.santosp@gmail.com>
2022-06-06 18:18:23 -0300

    curso udemy 22 e 23

---
+++ Curso_Udemy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+static void Aula22()
+{
+    string x = Console.ReadLine();
+
+    string[] vetor = x.Split(' ');
+
+    Console.WriteLine(vetor[1]);
+}
+static void Aula23()
+{
+    string[] dadosPessoa = Console.ReadLine().Split(' ');
+
+    string nome = dadosPessoa[0];
+    char sexo = char.Parse(dadosPessoa[1]);
+    int idade = int.Parse(dadosPessoa[2]);
+    double altura = double.Parse(dadosPessoa[3], CultureInfo.InvariantCulture);
+
+    Console.WriteLine($"{nome} se identifica com o sexo {sexo}, tem {idade} anos e {altura.ToString("F2", CultureInfo.InvariantCulture)}m de altura.");
+}
+
+Aula23();
+
+    
